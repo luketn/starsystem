2e117bb0750c93aa33fc49629a0a2fd2c48855f3
lthompson <lthompson@infomedia.com.au>
2018-08-25 18:31:21 +1000

    Add a name to a planet

--- src/main/java/com/mycodefu/starsystem/Planet.java
+++ src/main/java/com/mycodefu/starsystem/Planet.java
@@ -4,6 +4,11 @@ import java.awt.*;
 import java.util.Random;
 
 public class Planet extends Body {
+    private final String name;
+    private final Color planetColor;
+    private final int diameter;
+    private final boolean rings;
+    private final Color ringColor;
     private final Star star;
     private final int distanceFromStar;
 
@@ -11,11 +16,32 @@ public class Planet extends Body {
 
     private static final Random random = new Random();
 
-    private Planet(Color planetColor, int diameter, Star star, int distanceFromStar, double angle) {
+    private Planet(String name, Color planetColor, int diameter, boolean rings, Color ringColor, Star star, int distanceFromStar, double angle) {
         super(planetColor, diameter, calculateX(star, distanceFromStar, angle), calculateY(star, distanceFromStar, angle));
+        this.name = name;
+        this.planetColor = planetColor;
+        this.diameter = diameter;
+        this.rings = rings;
+        this.ringColor = ringColor;
         this.star = star;
         this.distanceFromStar = distanceFromStar;
         this.angle = angle;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Color getPlanetColor() {
+        return planetColor;
+    }
+
+    public Color getRingColor() {
+        return ringColor;
+    }
+
+    public double getAngle() {
+        return angle;
     }
 
     private static int calculateX(Star star, int distanceFromStar, double angle) {
@@ -39,9 +65,31 @@ public class Planet extends Body {
         int distanceFromStar = 50 * numberOutFromStar;
         int planetDiameter = 15 + random.nextInt(20);
         double angle = random.nextDouble() * 360d;
-        return new Planet(planetColor, planetDiameter, star, distanceFromStar, angle);
+        Color ringColor = new Color(100 + random.nextInt(155), 100 + random.nextInt(155), 100 + random.nextInt(155));
+
+        random.nextBoolean();
+
+        String name = RandomNameGenerator.generateName();
+
+        return new Planet(name, planetColor, planetDiameter, random.nextBoolean(), ringColor, star, distanceFromStar, angle);
     }
 
+    @Override
+    public void draw(Graphics g) {
+        super.draw(g);
+
+        if (rings) {
+            int startX = this.x - diameter / 2 - 5;
+            int startY = this.y - diameter / 2 - 5;
+            Color color = ringColor;
+            for (int i = 0; i < 5; i++) {
+                color = color.darker();
+                g.setColor(color);
+
+                g.drawOval(startX - i, startY - i, diameter + 10 + i * 2, diameter + 10 + i * 2);
+            }
+        }
+    }
 
     public void rotate(double increment) {
         if (this.angle < (360 - increment)) {
